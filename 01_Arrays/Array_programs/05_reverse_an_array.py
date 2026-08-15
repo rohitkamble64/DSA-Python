086c6258fe173a4351f0526bd9037d72e87c9314
Rohit Kamble <rohitkamble6405@gmail.com>
2026-08-15 18:07:38 +0530

    Add program to reverse an array

---
+++ 01_Arrays/Array_programs/05_reverse_an_array.py
@@ -0,0 +1,13 @@
+# Reversing an  array using two pointer approach
+def reverseArray(arr):
+    start = 0
+    end = len(arr) - 1
+
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+
+arr = [1, 2, 3, 4, 5]
+reverseArray(arr)
+print(arr)
